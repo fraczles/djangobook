dab617b2d6b97728336a191efb524b0bbe8e694b
Alex Fraczak <fraczakalex@gmail.com>
2014-09-07 16:07:31 -0400

    admin changelist customization

--- djangobook/books/admin.py
+++ djangobook/books/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from books.models import Publisher, Author, Book
 
+#Allows customization of fields visable on admin "change list"
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email')
+    search_fields = ('first_name', 'last_name')
+class BookAdmin(admin.ModelAdmin):
+    ist_display = ('title', 'publisher', 'publication_date')
+    list_filter = ('publication_date',)
+
+    
 admin.site.register(Publisher)
-admin.site.register(Author)
-admin.site.register(Book)
+# "Register the Author model with the AuthorAdmin options."
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book, BookAdmin)
 
